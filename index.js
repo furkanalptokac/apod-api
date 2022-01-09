10f751144414b3b6086d9c94d0c244298d2edf3d
Furkan Alp Tokaç <furkanalptokac@gmail.com>
2022-01-09 12:59:15 +0300

    cronjob feature added, endpoint returns empty object. todo added.
    Signed-off-by: Furkan Alp Tokaç <furkanalptokac@gmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import download from 'image-downloader';
 import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
+import cron from 'node-cron';
 
 const app = express();
 const router = express.Router();
@@ -20,28 +21,26 @@ app.use(express.urlencoded({ extended: true }));
 const imgpath = './img';
 
 if (!fs.existsSync(imgpath)) {
-    fs.mkdir(imgpath);
+    fs.mkdir(imgpath, () => {});
 }
 
-router.get('/', (req, res) => {
-    res.send('ADOP API Index');
+cron.schedule('57 12 * * *', () => {
+    getADOP();
 });
 
-router.get('/api/', (req, res) => {
-    // TODO: Cronjob
-
+const getADOP = () => {
     const downloadImage = (response) => {
         const dir = `${imgpath}/${response.data.date}/`;
         
-        if (!fs.existsSync(dir)) {
-            fs.mkdir(dir);
+        if (!fs.existsSync(dir)){
+            fs.mkdir(dir, () => {});
         }
-
+        
         const options = {
             url: response.data.url,
             dest: dir,
         };
-    
+
         const optionshd = {
             url: response.data.hdurl,
             dest: dir,
@@ -50,23 +49,35 @@ router.get('/api/', (req, res) => {
         download.image(options);
         download.image(optionshd);
         fs.writeFileSync(`${dir}/${response.data.date}.json`, JSON.stringify(response.data));
-    };
+    }
 
     axios({
         url: `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`,
-        method: 'get'
+        method: 'get',
     }).then((response) => {
         downloadImage(response);
-        res.json({
+        return {
             message: 'Image downloaded',
-            url: response.data.url
-        });
+            url: response.data.url,
+        };
     }).catch((err) => {
         console.error(err);
-        res.send('Error.');
+        return {
+            message: 'Error'
+        }
     });
+};
+
+
+router.get('/', (req, res) => {
+    res.send('ADOP API Index');
 });
 
+router.get('/api/', (req, res) => {
+    // TODO: Returns empty object
+    const response = getADOP();
+    res.send(response);
+});
 app.listen(app.get('port'), () => {
     console.log(`Server is listening on http://localhost:${app.get('port')}`);
 });
